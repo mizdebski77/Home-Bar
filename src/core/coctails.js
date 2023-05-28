c4342af089952a29d64fbc4ecaf181139d13fabc
Marcin Izdebski <mizdebski123@gmail.com>
2023-05-28 22:48:35 +0200

    Added array with coctails

---
+++ src/core/coctails.js
@@ -0,0 +1,400 @@
+export const gin = [
+    {
+        id: 1,
+        name: 'Gimlet',
+        image: 'https://www.simplyrecipes.com/thmb/d6a-ci1K1GNTKZ51sR3x5eY-u4c=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2020__08__Gimlet-Cocktail-LEAD-1-c265ed3ce162459ea04b3e795ace26d8.jpg',
+        ingredients: ['gin', 'sok z limonki', 'syrop cukrowy']
+    },
+    {
+        id: 2,
+        name: 'Rickey',
+        image: 'https://www.acouplecooks.com/wp-content/uploads/2021/03/Lime-Rickey-003.jpg',
+        ingredients: ['gin', 'sok z limonki', 'woda sodowa']
+    },
+    {
+        id: 3,
+        name: 'Basil Smash',
+        image: 'https://www.acouplecooks.com/wp-content/uploads/2021/05/Gin-Basil-Smash-001.jpg',
+        ingredients: ['gin', 'sok z cytryny', 'syrop cukrowy', 'liście bazylii']
+    },
+    {
+        id: 4,
+        name: 'Orange Blossom',
+        image: 'https://www.liquor.com/thmb/tjao4u4XmY9gDDNpLF7zP1ECsSY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/orange-blossom-720x720-recipe-70b85d05c34a4bc381e359cf693c2f79.jpg',
+        ingredients: ['wódka', 'likier pomarańczowy', 'sok z pomarańczy']
+    },
+    {
+        id: 5,
+        name: 'Baby Blue Martini',
+        image: 'https://kulinarneprzeboje.pl/wp-content/uploads/2020/01/drink-baby-blue-martini-768x1155.jpg',
+        ingredients: ['wódka', 'curacao blue', 'sok z ananasa']
+    },
+    {
+        id: 6,
+        name: 'Gin Fix',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/gin-fix.jpg',
+        ingredients: ['gin', 'syrop cukrowy', 'sok z cytryny']
+    },
+    {
+        id: 7,
+        name: 'Bikini Martini',
+        image: 'https://kokteli.hr/wp-content/uploads/2021/05/Bikini-Martini.jpg',
+        ingredients: ['wódka', 'malibu', 'sok ananasowy', 'sok żurawinowy']
+    },
+    {
+        id: 8,
+        name: 'Southside Rickey',
+        image: 'https://www.tasteofhome.com/wp-content/uploads/2019/11/mojito-southside-shutterstock_1412357489.jpg?resize=768,576',
+        ingredients: ['rum', 'sok z limonki', 'mięta', 'woda sodowa']
+    },
+    {
+        id: 9,
+        name: 'Ink Martini',
+        image: 'https://cocktailsandbars.com/wp-content/uploads/2016/10/blacktopus-martini-cocktailsandbars-IMG_094689-1.jpg',
+        ingredients: ['wódka', 'sok z cytryny', 'syrop cukrowy', 'blued curacao']
+    },
+    {
+        id: 10,
+        name: 'Southside',
+        image: 'https://www.acouplecooks.com/wp-content/uploads/2020/03/Southside-Cocktail-001.jpg',
+        ingredients: ['gin', 'sok z limonki', 'syrop cukrowy', 'mięta']
+    },
+    {
+        id: 11,
+        name: 'Gin & Tonic',
+        image: 'https://www.thespruceeats.com/thmb/r7Zjvziw0mGDawZw1ZMGAA79gEU=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/gin-and-tonic-recipe-759300-hero-01-aa12e6504f944c54b8b9c589cc1d0ac6.jpg',
+        ingredients: ['gin', 'tonic']
+    },
+    {
+        id: 12,
+        name: 'Pink Hound',
+        image: 'https://mojbar.pl/wp-content/uploads/2020/01/PINK-HOUD.jpg',
+        ingredients: ['wódka', 'sok grejpfrutowy']
+    },
+];
+
+
+export const vodka = [
+    {
+        id: 13,
+        name: 'Long Island Iced Tea',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/long-island-iced-tea.jpg',
+        ingredients: ['wódka', 'rum', 'tequila', 'gin', 'triple sec', 'sok z cytryny', 'coca-cola']
+    },
+    {
+        id: 14,
+        name: 'Sex on the Beach',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/sex-on-the-beach.jpg',
+        ingredients: ['wódka', 'likier brzoskwiniowy', 'sok pomarańczowy', 'sok żurawinowy']
+    },
+    {
+        id: 15,
+        name: 'Wódka Sour',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/05/vodka-sour.jpg',
+        ingredients: ['wódka', 'sok z cytryny', 'syrop cukrowy']
+    },
+    {
+        id: 16,
+        name: '',
+        image: 'https://mojbar.pl/wp-content/uploads/2018/12/JUNGLE-JUICE.jpg',
+        ingredients: ['wódka', 'rum', 'tequila', 'sok pomarańczowy', 'sok ananasowy', 'sok żurawinowy', 'sprite']
+    },
+    {
+        id: 17,
+        name: 'Screwdriver',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/screwdriver.jpg',
+        ingredients: ['wódka', 'sok pomarańczowy']
+    },
+    {
+        id: 18,
+        name: 'White Russian',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/05/white-russian.jpg',
+        ingredients: ['wódka', 'kahlua', 'śmietanka']
+    },
+    {
+        id: 19,
+        name: 'Black Russian',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/03/black-russian.jpg',
+        ingredients: ['wódka', 'kahlua']
+    },
+    {
+        id: 20,
+        name: 'Hard Lemonade',
+        image: 'https://mojbar.pl/wp-content/uploads/2019/01/HARD-LEMONADE.jpg',
+        ingredients: ['wódka', 'sok z cytryny', 'syrop cukrowy', 'woda gazowana']
+    },
+    {
+        id: 21,
+        name: 'Vodka Collins',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/05/vodka-collins.jpg',
+        ingredients: ['wódka', 'sok z cytryny', 'syrop cukrowy', 'woda gazowana']
+    },
+    {
+        id: 22,
+        name: 'Hawaiian Seabreeze',
+        image: 'https://mojbar.pl/wp-content/uploads/2019/01/HAWAIIAN-SEABREEZE.jpg',
+        ingredients: ['wódka', 'sok ananasowy', 'sok żurawinowy', 'sok grejpfrutowy']
+    },
+    {
+        id: 23,
+        name: 'Woo-Woo',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/05/woo-woo.jpg',
+        ingredients: ['wódka', 'likier brzoskwiniowy', 'sok żurawinowy']
+    },
+    {
+        id: 24,
+        name: 'Momo Special',
+        image: 'https://mojbar.pl/wp-content/uploads/2019/11/MOMO-SPECJAL.jpg',
+        ingredients: ['wódka', 'likier melonowy', 'sok ananasowy']
+    },
+    {
+        id: 25,
+        name: 'Caipiroska',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/caipirosca.jpg',
+        ingredients: ['wódka', 'limonka', 'syrop cukrowy']
+    },
+    {
+        id: 26,
+        name: 'Madras',
+        image: 'https://mojbar.pl/wp-content/uploads/2019/11/MADRAS.jpg',
+        ingredients: ['wódka', 'sok pomarańczowy', 'sok żurawinowy']
+    },
+    {
+        id: 27,
+        name: 'Cape Codder',
+        image: 'https://mojbar.pl/wp-content/uploads/2020/07/cape-codder.jpg',
+        ingredients: ['wódka', 'sok żurawinowy']
+    },
+];
+
+
+export const whiskey = [
+    {
+        id: 28,
+        name: 'Whisky Sour',
+        image: 'https://mojbar.pl/wp-content/uploads/2018/05/whisky-sour-2-1.jpg',
+        ingredients: ['whisky', 'sok z cytryny', 'syrop cukrowy']
+    },
+    {
+        id: 29,
+        name: 'Mint Julep',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/mint-julep.jpg',
+        ingredients: ['bourbon', 'syrop miętowy', 'mięta', 'woda gazowana']
+    },
+    {
+
+        id: 30,
+        name: 'Mississippi Schnapper',
+        image: 'https://mojbar.pl/wp-content/uploads/2019/11/MISSISSIPPI-SCHNAPPER.jpg',
+        ingredients: ['whisky', 'sok z cytryny', 'syrop cukrowy', 'sok pomarańczowy']
+    },
+    {
+        id: 31,
+        name: 'Pineapple Blossom',
+        image: 'https://mojbar.pl/wp-content/uploads/2020/01/PINEAPPLE-BLOSSOM.jpg',
+        ingredients: ['rum', 'sok ananasowy', 'sok żurawinowy', 'sok pomarańczowy']
+    },
+    {
+        id: 32,
+        name: 'New Yorker',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/new-yorker.jpg',
+        ingredients: ['wódka', 'sok żurawinowy', 'sok z limonki', 'syrop cukrowy']
+    },
+    {
+        id: 33,
+        name: 'Bessie & Jessie',
+        image: 'https://mojbar.pl/wp-content/uploads/2018/12/BESSIE-AND-JESSIE.jpg',
+        ingredients: ['wódka', 'likier malibu', 'sok z ananasa', 'sok żurawinowy']
+    },
+    {
+        id: 64,
+        name: 'Aggravation',
+        image: 'https://mojbar.pl/wp-content/uploads/2018/12/AGGRAVATION.jpg',
+        ingredients: ['wódka', 'likier malibu', 'sok z ananasa', 'sok żurawinowy']
+    },
+];
+
+
+export const rum = [
+    {
+        id: 34,
+        name: 'Cuba Libre',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/cuba-libre.jpg',
+        ingredients: ['rum', 'cola', 'sok z limonki']
+    },
+    {
+        id: 35,
+        name: 'Mojito',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/luxury-mojito.jpg',
+        ingredients: ['rum', 'sok z limonki', 'syrop cukrowy', 'mięta', 'soda']
+    },
+    {
+        id: 36,
+        name: 'Daiquiri',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/daiquiri.jpg',
+        ingredients: ['rum', 'sok z limonki', 'syrop cukrowy']
+    },
+    {
+        id: 37,
+        name: 'Rum Sour',
+        image: 'https://mojbar.pl/wp-content/uploads/2020/07/rum-sour.jpg',
+        ingredients: ['rum', 'sok z cytryny', 'syrop cukrowy']
+    },
+    {
+        id: 38,
+        name: 'Strawberry Daiquiri',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/strawberry-daiquiri.jpg',
+        ingredients: ['rum', 'sok truskawkowy', 'sok z limonki', 'syrop cukrowy']
+    },
+    {
+        id: 39,
+        name: 'Pedro Collins',
+        image: 'https://mojbar.pl/wp-content/uploads/2020/01/PEDRO-COLLINS.jpg',
+        ingredients: ['rum', 'sok z cytryny', 'syrop cukrowy', 'soda']
+    },
+    {
+        id: 40,
+        name: 'Rum Special',
+        image: 'https://mojbar.pl/wp-content/uploads/2020/07/Rum-Special.jpg',
+        ingredients: ['rum', 'likier malibu', 'sok z ananasa']
+    },
+    {
+        id: 41,
+        name: 'Peach Daiquiri',
+        image: 'https://mojbar.pl/wp-content/uploads/2020/01/PEACH-DAIQUIRI-FROZEN.jpg',
+        ingredients: ['rum', 'sok brzoskwiniowy', 'sok z limonki', 'syrop cukrowy']
+    },
+    {
+        id: 43,
+        name: 'Caiprissima',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/caipirissima.jpg',
+        ingredients: ['rum', 'limonka', 'syrop cukrowy']
+    },
+    {
+        id: 44,
+        name: 'Pineapple Fizz',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/pineapple-fizz.jpg',
+        ingredients: ['rum', 'sok ananasowy', 'sok z limonki', 'soda']
+    },
+    {
+        id: 46,
+        name: 'Aku Aku',
+        image: 'https://mojbar.pl/wp-content/uploads/2020/09/Aku-Aku.jpg',
+        ingredients: ['rum', 'likier maraskino', 'sok z cytryny', 'sok z ananasa']
+    },
+    {
+        id: 47,
+        name: 'Derby Daiquiri',
+        image: 'https://mojbar.pl/wp-content/uploads/2020/09/Derby-Daiquiri.jpg',
+        ingredients: ['rum', 'sok z limonki', 'syrop cukrowy', 'mięta', 'sok malinowy']
+    },
+    {
+        id: 48,
+        name: 'Bajito',
+        image: 'https://mojbar.pl/wp-content/uploads/2020/09/Bajito.jpg',
+        ingredients: ['rum', 'sok z limonki', 'mięta', 'syrop cukrowy', 'woda gazowana']
+    },
+    {
+        id: 49,
+        name: 'Basil and Honey',
+        image: 'https://mojbar.pl/wp-content/uploads/2020/09/Basil-and-Honey-Daiquiri.jpg',
+        ingredients: ['rum', 'sok z limonki', 'mięta', 'syrop cukrowy', 'miód']
+    },
+];
+
+export const tequila = [
+    {
+        id: 50,
+        name: 'Acapulco',
+        image: 'https://mojbar.pl/wp-content/uploads/2018/12/h.jpg',
+        ingredients: ['tequila', 'sok pomarańczowy', 'sok z cytryny', 'syrop grenadine']
+    },
+    {
+        id: 51,
+        name: 'Tequila Sunrise',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/tequilla-sunrise.jpg',
+        ingredients: ['tequila', 'sok pomarańczowy', 'syrop grenadine']
+    },
+    {
+        id: 52,
+        name: 'Cactus Jack',
+        image: 'https://mojbar.pl/wp-content/uploads/2018/05/CACTUS-JACK.jpg',
+        ingredients: ['tequila', 'likier melonowy', 'sok ananasowy', 'sok z cytryny']
+    },
+    {
+        id: 53,
+        name: 'Truskawkowa Margarita',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/strawberry-margarita.jpg',
+        ingredients: ['tequila', 'likier truskawkowy', 'sok z cytryny', 'sok truskawkowy', 'syrop cukrowy']
+    },
+    {
+        id: 54,
+        name: 'Tequila Sour',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/tequilla-sunrise.jpg',
+        ingredients: ['tequila', 'sok z cytryny', 'syrop cukrowy']
+    },
+    {
+        id: 55,
+        name: 'Mexican Mule',
+        image: 'https://mojbar.pl/wp-content/uploads/2019/11/MEXICAN-MULE.jpg',
+        ingredients: ['tequila', 'sok z limonki', 'piwo imbirowe']
+    },
+    {
+        id: 56,
+        name: 'Pineapple Margaritas',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/pineapple-margarita.jpg',
+        ingredients: ['tequila', 'likier maraskino', 'sok ananasowy', 'sok z cytryny']
+    },
+    {
+        id: 57,
+        name: 'Mayan',
+        image: 'https://mojbar.pl/wp-content/uploads/2019/11/MAYAN.jpg',
+        ingredients: ['tequila', 'sok z limonki', 'syrop cukrowy', 'sok żurawinowy']
+    },
+    {
+        id: 58,
+        name: 'Frozen Peach Margarita',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/peach-margarita-frozen.jpg',
+        ingredients: ['tequila', 'sok brzoskwiniowy', 'sok z cytryny', 'syrop cukrowy']
+    },
+    {
+        id: 59,
+        name: 'Mexican Surfer',
+        image: 'https://mojbar.pl/wp-content/uploads/2017/04/margarita.jpg',
+        ingredients: ['tequila', 'sok z limonki', 'syrop agawy', 'sok pomarańczowy']
+    },
+    {
+        id: 60,
+        name: 'Bad Eagle Martini',
+        image: 'https://mojbar.pl/wp-content/uploads/2019/10/BALD-EAGLE-MARTINI.jpg',
+        ingredients: ['tequila', 'sok z cytryny', 'syrop cukrowy']
+    },
+];
+
+
+export const shots = [
+    {
+        id: 61,
+        name: 'Iguana',
+        image: 'https://mojbar.pl/wp-content/uploads/2018/12/IGUANA.jpg',
+        ingredients: ['tequila', 'likier melonowy', 'sok pomarańczowy', 'sok z cytryny']
+    },
+    {
+        id: 62,
+        name: 'Kamikaze Shot',
+        image: 'https://koktajl.tv/file/Kamikaze-shoty_9cdbc9a52652b5fb1ab979c424693848ceefede2.jpg',
+        ingredients: ['wódka', 'likier blue curaçao', 'sok z cytryny']
+    },
+    {
+        id: 63,
+        name: 'All Fall Down',
+        image: 'https://mojbar.pl/wp-content/uploads/2019/10/ALL-FALL-DOWN.jpg',
+        ingredients: ['wódka', 'likier malibu', 'likier brzoskwiniowy', 'sok ananasowy', 'sok żurawinowy']
+    },
+    {
+        id: 64,
+        name: 'Amigo Shot',
+        image: 'https://mojbar.pl/wp-content/uploads/2019/10/AMIGO-SHOT.jpg',
+        ingredients: ['tequila', 'likier kawowy']
+    }
+];
