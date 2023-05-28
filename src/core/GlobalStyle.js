bf975d23cf8f1e859ecf30d181b7a7132e7bbfa8
Marcin Izdebski <mizdebski123@gmail.com>
2023-05-28 22:27:37 +0200

    Initial commit

---
+++ src/core/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+    box-sizing: border-box;
+  }
+  
+  *,
+  ::after,
+  ::before {
+    box-sizing: inherit;
+  }
+  
+  body {
+    background: ${({theme}) => theme.color.mainColor};
+    color: ${({theme}) => theme.color.fontColor};
+    font-family: 'Roboto Slab', serif;
+    padding: 20px;
+  }`;
---
+++ src/core/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+    color: {
+        mainColor: "#111111",
+        secondColor: "#07252D",
+        thirdColor: "#b09c3c",
+        fontColor: "#C2BFAF",
+    },
+
+    breakPoint: {
+        mobileMax: 900,
+    }
+}
